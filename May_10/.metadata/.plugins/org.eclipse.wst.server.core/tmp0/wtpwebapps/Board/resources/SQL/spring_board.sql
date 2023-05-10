8427f9457a5f67ab8cd27e38660014991acfb024
MIN-DONGHYUN <mdh2057@naver.com>
2023-05-10 17:15:40 +0900

    2023.05.10 form, ajax, tiles

---
+++ May_10/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Board/resources/SQL/spring_board.sql
@@ -0,0 +1,20 @@
+------ ***** spring 기초 ***** -----
+
+show user;
+-- USER이(가) "MYMVC_USER"입니다.
+
+create table spring_test
+(no         number
+,name       varchar2(100)
+,writeday   date default sysdate
+);
+-- Table SPRING_TEST이(가) 생성되었습니다.
+
+
+select no, name, to_char(writeday, 'yyyy-mm-dd hh24:mi:ss') AS writeday
+from spring_test
+order by writeday desc;
+
+
+delete form spring_test
+where NO = 101;
